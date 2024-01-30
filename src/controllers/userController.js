34718c1a6df934bb3d0b98f8a2ff406500e22c16
YeonsuJeong <runnway@naver.com>
2024-01-30 23:11:06 +0900

    pug if condition

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
-export const join = (req, res) => res.render("join");
-export const edit = (req, res) => res.render("edit");
-export const remove = (req, res) => res.render("remove");
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
-export const see = (req, res) => res.render("see");
+export const join = (req, res) => res.send("join");
+export const edit = (req, res) => res.send("edit");
+export const remove = (req, res) => res.send("remove");
+export const login = (req, res) => res.send("login");
+export const logout = (req, res) => res.send("logout");
+export const see = (req, res) => res.send("see");
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,13 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+const fakeUser = {
+  username: "Nicolas",
+  loggedIn: true,
+};
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", fakeUser });
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
-export const search = (req, res) =>
-  res.render("search", { pageTitle: "Search" });
-export const upload = (req, res) =>
-  res.render("upload", { pageTitle: "Upload" });
+export const search = (req, res) => res.send("search", { pageTitle: "Search" });
+export const upload = (req, res) => res.send("upload", { pageTitle: "Upload" });
 export const deleteVideo = (req, res) =>
-  res.render("delete", { pageTitle: "Delete" });
+  res.send("delete", { pageTitle: "Delete" });
